ae545967204387d0802653fba5ee7bea5be9aa01
kkadir <kadir.kilicoglu@outlook.com>
2023-04-12 18:13:29 +0300

    Gradlew permissions

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -1,4 +1,5 @@
 package tr.edu.metu.sm703;
+
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import java.util.Collections;
@@ -9,6 +10,6 @@ public class HomeController {
 
     @Get
     public Map<String, Object> index() {
-        return Collections.singletonMap("message", "Hello World");
+        return Collections.singletonMap("message", "Hello World New");
     }
 }
